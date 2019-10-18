ab4829dde13140b4861201ee78fd94da23eb3d8e
Valkiria Rodrigues <valkiria.asaffe2412@gmail.com>
2019-10-17 22:35:06 -0400

    continuação aula 03

--- AULA03/src/js/index.js
+++ AULA03/src/js/index.js
@@ -19,7 +19,64 @@ function salvarContato(event){
     //adiciona o contato no nosso banco de dados(no final do vetor)
     contatos.push(contato);
 
-    console.log(contatos)
+    //invoca a renderização da tabela
+    renderizarTabelaContatos();
 
 
+}
+
+function renderizarTabelaContatos(){
+    if(contatos.length > 0 ){
+        let areaListagemContatos=document.getElementById('ListagemContatos');
+
+        /**
+         * cria a tabela
+         */
+         let tabela= document.createElement('table');
+
+         /**
+          * limpa a area de listagem
+          */
+         areaListagemContatos.innerHTML='';
+
+         /**
+          * cria o cabeçalho da tabela
+          */
+         let cabecalho=document.createElement('thead');
+         let linhaCabecalho=document.createElement('tr');
+         let colunaNome=document.createElement('th');
+         colunaNome.innerText= 'Nome'; //texto que vai dentro da tag
+         let colunaTelefone=document.createElement('th');
+         colunaTelefone.innerText= 'Telefone';
+         let colunaEmail=document.createElement('th');
+         colunaEmail.innerText= 'Email';
+         let colunaDataNasc=document.createElement('th');
+         colunaDataNasc.innerText= 'Data Nasc';
+         
+         /**
+          * adiciona as colunas na linha do cabeçalho
+          */
+         linhaCabecalho.appendChild(colunaNome);
+         linhaCabecalho.appendChild(colunaTelefone);
+         linhaCabecalho.appendChild(colunaEmail);
+         linhaCabecalho.appendChild(colunaDataNasc);
+
+         /**
+          * adicionar a linha do cabeçalho no cabeçalho
+          */
+         cabecalho.appendChild(linhaCabecalho);
+
+         /**
+          * adiciona o cabeçalho dentro da tabela
+          */
+         tabela.appendChild(cabecalho);
+
+         /**
+          * adiciona a tabela dentro da area de listagem
+          */
+         areaListagemContatos.appendChild(tabela);
+
+
+
+    }
 }
